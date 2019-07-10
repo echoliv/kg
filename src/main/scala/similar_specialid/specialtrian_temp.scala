f4fdc1c27fb69126c3d800f340caf1d03ffd2222
liulu <liulu@kugou.net>
2019-07-10 18:33:55 +0800

    first

--- src/main/scala/similar_specialid/specialtrian_temp.scala
+++ src/main/scala/similar_specialid/specialtrian_temp.scala
@@ -6,22 +6,20 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.feature.{StringIndexer, VectorIndexer}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.linalg.Vectors
+//import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by bearlin on 2015/11/19.
   */
-object specialSimilarityTrain {
+object specialSimilarityTrain2 {
   def main(args: Array[String]){
-    val conf = new SparkConf()
-      .setAppName("specialSimilarityTrain")
-      .set("spark.rdd.compress", "true")
-      .set("spark.storage.memoryFraction", "0.2")
-      .set("spark.executor.cores", "2")
-      .set("spark.default.parallelism", "320")
-      .set("spark.shuffle.safetyFraction", "0.5")
-      .set("spark.executor.extraJavaOptions","-XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:PermSize=256M -XX:MaxPermSize=256M")
+
+
+
+    System.setProperty("hadoop.home.dir", "D:\\hadoop-2.7.7");
+    val conf = new SparkConf().setAppName("k-means").setMaster("local").set("spark.driver.allowMultipleContexts", "true");
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
@@ -30,12 +28,12 @@ object specialSimilarityTrain {
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
     //读取训练数据
-    val sample_file = args(0)
-    val pairs_file = args(1)
+    val sample_file = "file\\special2"
+    val pairs_file = "file\\special1"
     //val sample_file = "/user/hive/warehouse/analyse.db/recommendation_specialsim_sample/*/*"
     //val pairs_file = "/user/hive/warehouse/analyse.db/recommendation_specialsim_pairs/dt=2016-10-08/*"
     //读取训练参数
-    val resultFile = args(2)
+    val resultFile = "file\\v7.txt"
     //val tagCandidateFile = args(3)
 
     //val resultFile = "/user/hive/warehouse/analyse.db/recommendSpecial/"
@@ -50,13 +48,13 @@ object specialSimilarityTrain {
 
 
 
-    val trainingSamples = sc.textFile(sample_file).map{ line =>
+    val trainingSamples1 = sc.textFile(sample_file).map{ line =>
       val s = line.split('|')
-      val colle = s(2).split(',').map(x => x.toInt).toList
+      val colle = s(2).split(',').map(x => x).toList
       var label = 0.0
       var mark = 1.0
       val r = scala.util.Random
-      if (colle.contains(s(3).toInt)){
+      if (colle.contains(s(3))){
         label = 1.0
       } else {
         label = 0.0
@@ -64,8 +62,16 @@ object specialSimilarityTrain {
           mark = 0.0
         }
       }
-      (label,s.slice(4, s.size).map(_.toDouble),mark,s(1).toInt,s(3).toInt)
-    }.filter(x => x._3==1.0).map(x=>(x._1,Vectors.dense(x._2))).toDF("label","features")
+      (label,s.slice(4, s.size-1).map(_.toDouble),mark)
+    }
+
+    val temp2 =trainingSamples1.map(x=>(x._1,x._2)).map {
+      x =>
+        println(x._1)
+        for (i <- 0 to x._2.length-1) {
+          println(x._2(i))
+        }
+    }
 
 
     val predict_pairs = sc.textFile(pairs_file).map{ line =>
@@ -73,6 +79,7 @@ object specialSimilarityTrain {
       (s(0) + '_' + s(1),s.slice(2, s.size).map(_.toDouble))
     }.map(x=>(x._1,Vectors.dense(x._2))).toDF("pairs","features")
     //对类标和特征进行编码，变成pipleline可读取的格式
+    val trainingSamples = trainingSamples1.filter(x => x._3==1.0).map(x=>(x._1,Vectors.dense(x._2))).toDF("label","features")
     val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(trainingSamples)
 
     //多次训练并测试
@@ -101,10 +108,21 @@ object specialSimilarityTrain {
     val pipeline = new Pipeline().setStages(Array(labelIndexer, rf))
     val model = pipeline.fit(trainingSamples)
     //对候选对进行预测，并使用标签集进行过滤
+
+    val temp1 = model.transform(predict_pairs).select("pairs","probability").rdd.map{x=>
+      val x1=x.get(0).toString
+      val x2 = x.get(1).toString
+      println(x1+' '+x2)
+      (x1,x2)
+    }.collect()
+    for(i<-0 to temp1.length-1){
+      println(temp1(i)._1 +' '+temp1(i)._2)
+    }
+
     val predictions = model.transform(predict_pairs).select("pairs","probability").map{line =>
       val score = line.get(1).toString.split('[')(1).split(']')(0).split(',')(1).toDouble
       (line.get(0).toString.split('_')(0),line.get(0).toString.split('_')(1),score)
-    }.map(x => (x._1.toInt,(x._2,x._3))).rdd.groupByKey().flatMap{x =>
+    }.map(x => (x._1.toString,(x._2,x._3))).rdd.groupByKey().flatMap{x =>
       val resultK = 18
       val specialid = x._1
       val recommendSpecial = x._2.toSeq.sortWith(_._2 > _._2)
@@ -129,7 +147,8 @@ object specialSimilarityTrain {
     val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
     try { hdfs.delete(new org.apache.hadoop.fs.Path(resultFile), true) } catch { case _ : Throwable => { } }
 
-    predictions.map( x => x._1.toString + "|" + x._2.toString + "|" + x._3.toString).repartition(5).saveAsTextFile(resultFile, classOf[GzipCodec])
+//    predictions.map( x => x._1.toString + "|" + x._2.toString + "|" + x._3.toString).repartition(5).saveAsTextFile(resultFile, classOf[GzipCodec])
+    predictions.map(x =>x._1.toString +"|" +x._2.toString +"|" +x._3.toString).saveAsTextFile(resultFile)
 
     //预测并输出
 
